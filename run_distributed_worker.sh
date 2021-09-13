55e48ed3fb00da09c6cb878115d5a1e961beb2a0
sabyadi <5201_sad@mail.ru>
2021-09-13 10:55:32 +0200

    Improve docs

--- run_distributed_worker.sh
+++ run_distributed_worker.sh
@@ -1,4 +1,4 @@
-MASTER_HOST=127.0.0.1
+MASTER_HOST=$1
 rm worker*.log
 nohup locust -f load_test.py --worker --master-host=$MASTER_HOST > worker1.log &
 nohup locust -f load_test.py --worker --master-host=$MASTER_HOST > worker2.log &
