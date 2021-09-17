687a81303c9788adad4cea554ab883914b5ea87c
Carlos Castro <carlos.castro@mongodb.com>
2021-09-17 09:07:47 +0100

    add MONGO_URI as an environment variable; remove multiple workers;

--- run_distributed_worker.sh
+++ run_distributed_worker.sh
@@ -1,9 +1,3 @@
 MASTER_HOST=$1
 rm worker*.log
 nohup locust -f load_test.py --worker --master-host=$MASTER_HOST > worker1.log &
-nohup locust -f load_test.py --worker --master-host=$MASTER_HOST > worker2.log &
-#nohup locust -f load_test.py --worker --master-host=$MASTER_HOST > worker3.log &
-#nohup locust -f load_test.py --worker --master-host=$MASTER_HOST > worker4.log &
-#nohup locust -f load_test.py --worker --master-host=$MASTER_HOST > worker5.log &
-#nohup locust -f load_test.py --worker --master-host=$MASTER_HOST > worker6.log &
-#nohup locust -f load_test.py --worker --master-host=$MASTER_HOST > worker7.log &
