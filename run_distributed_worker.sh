8d06671c09120a4615f55594d7e82116e9d13ca7
sabyadi <5201_sad@mail.ru>
2021-10-07 15:44:01 +0200

    Improve code readibility

--- run_distributed_worker.sh
+++ run_distributed_worker.sh
@@ -1,3 +1,4 @@
 MASTER_HOST=$1
 rm worker*.log
 nohup locust -f load_test.py --worker --master-host=$MASTER_HOST > worker1.log &
+nohup locust -f load_test.py --worker --master-host=$MASTER_HOST > worker2.log &
--- settings.py
+++ settings.py
@@ -1,5 +1,21 @@
-# Database name to use
-DB_NAME = 'sample'
-COLLECTION_NAME = 'documents'
-# cluster connection string
-CLUSTER_URL = f'mongodb+srv://user:password@something.mongodb.net/{DB_NAME}?retryWrites=true&w=majority'
+import os
+
+DEFAULTS = {'DB_NAME': 'sample',
+            'COLLECTION_NAME': 'documents',
+            'CLUSTER_URL': f'mongodb+srv://user:password@something.mongodb.net/sample?retryWrites=true&w=majority',
+            'DOCS_PER_BATCH': 100,
+            'INSERT_WEIGHT': 1,
+            'FIND_WEIGHT': 3,
+            'BULK_INSERT_WEIGHT': 1,
+            'AGG_PIPE_WEIGHT': 1}
+
+
+def init_defaults_from_env():
+    for key in DEFAULTS.keys():
+        value = os.environ.get(key)
+        if value:
+            DEFAULTS[key] = value
+
+
+# get the settings from the environment variables
+init_defaults_from_env()
