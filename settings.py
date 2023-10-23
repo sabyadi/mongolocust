af1e47e5cacc45d52bb7f2fa9da95a5d2e00e378
Parag Jain <paragjain@microsoft.com>
2023-10-23 12:19:28 +0530

    [BUG FIX] Some Enviorment variable should convert to INT explicitly

--- settings.py
+++ settings.py
@@ -14,7 +14,11 @@ def init_defaults_from_env():
     for key in DEFAULTS.keys():
         value = os.environ.get(key)
         if value:
-            DEFAULTS[key] = value
+            if key in ['DB_NAME', 'CLUSTER_URL']:
+                DEFAULTS[key] = value
+            else :
+                # Environment variables are being explicitly converted to integers from their string representation.
+                DEFAULTS[key] = int(value)
 
 
 # get the settings from the environment variables
